21c2770065f61dac2678452bb5746908504323c3
Salavat <malikov.salavat702@gmail.com>
2022-12-27 15:08:06 +0300

    2

---
+++ zadanie_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 50. Напишите программу, которая на вход 
+// принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("Введите номер строки");
+int rows= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int columns= Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int[5,5];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j]= new Random().Next(10);
+        Console.Write(matrix[i,j]+" ");
+    }
+     Console.WriteLine();
+}
+if (rows<matrix.GetLength(0)&&columns<matrix.GetLength(1))
+{
+    Console.Write(matrix[rows-1,columns-1]);
+}
+else
+{
+    Console.Write("Такого числа в массиве нет");
+}
